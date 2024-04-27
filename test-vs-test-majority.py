c3397ad83d4d8d3be7184d29e65eb10ccfaca69f
MeghanaPG <meghanapg3@gmail.com>
2024-04-27 19:44:46 -0400

    Committing the files

--- test-vs-test-majority.py
+++ test-vs-test-majority.py
@@ -47,10 +47,10 @@ def generate_bert_embeddings(text_data):
 
     return embeddings_np
 
-def split_dataset():
+def split_dataset(raw_input_file):
     print("Splitting the data")
     # splitting the dataset into features (X) and labels (y)
-    raw_input_file = "safest_labels_dataset.csv"
+    # raw_input_file = "safest_labels_dataset.csv"
     # raw_input_file = "majority_labels_dataset.csv"
     # raw_input_file = "least_safest_labels_dataset.csv"
 
@@ -63,7 +63,8 @@ def split_dataset():
     # dataset = dataset.head(15000)
     # dataset = dataset.sample(15000 , random_state = 42)
     print(len(dataset))
-    dataset = dataset.sample(500, random_state=42)
+    # dataset = dataset.sample(500, random_state=42)
+    dataset = dataset.sample(10000, random_state=42)
     ####### To remove rare classes ############
     # Count the occurrences of each class in 'y'
     class_counts = dataset['safety_label'].value_counts()
@@ -97,9 +98,7 @@ def split_dataset():
     test_size = 0.5
     random_state = 35
 
-    # Attempt to split the dataset
-    # while True:
-    #     try:
+
     X_train, X_temp, y_train, y_temp, embeddings_train, embeddings_temp = train_test_split(
         x, y_encoded, embeddings, test_size=test_size, random_state=random_state, stratify=y_encoded)
     X_val, X_test, y_val, y_test, embeddings_val, embeddings_test = train_test_split(
@@ -109,52 +108,42 @@ def split_dataset():
     return X_train, X_val, X_test, y_train, y_val, y_test, embeddings_train, embeddings_val, embeddings_test
 
 def main():
-    # splitting the dataset and obtain BERT embeddings
-    print("_______________________________________________________________________")
-    X_train, X_val, X_test, y_train, y_val, y_test, embeddings_train, embeddings_val, embeddings_test = split_dataset()
+    # Splitting the dataset and obtaining BERT embeddings
+    X_train, X_val, X_test, y_train, y_val, y_test, embeddings_train, embeddings_val, embeddings_test = split_dataset("majority_labels_dataset.csv")
+    X_train2, X_val2, X_test2, y_train2, y_val2, y_test2, embeddings_train2, embeddings_val2, embeddings_test2 = split_dataset("least_safest_labels_dataset.csv")
 
     input_shape = (768,)
     num_classes = 5
     bert_embeddings_file = np.load('bert_embeddings_file.npy')
 
-    # loading the CNN model here
+    # Loading the CNN model here
     model = create_cnn_model(num_classes, bert_embeddings_file)
 
-    # y_train_categorical = to_categorical(embeddings_val, num_classes=num_classes)
-    # y_val_categorical = to_categorical(y_val, num_classes=num_classes)
-
+    # Encoding the labels
     y_train_categorical = to_categorical(y_train, num_classes=num_classes)
     y_val_categorical = to_categorical(y_val, num_classes=num_classes)
-
     y_test_categorical = to_categorical(y_test, num_classes=num_classes)
 
-    # Evaluate the model using the one-hot encoded y_test
-    test_loss, test_accuracy = model.evaluate(embeddings_test, y_test_categorical)
-
+    # Training the model
     # history = model.fit(embeddings_train, y_train_categorical,
     #                     epochs=20,  # Specify the number of epochs
     #                     batch_size=32,  # Specify the batch size
     #                     validation_data=(embeddings_val, y_val_categorical))
 
+    # Evaluating the model on the first test dataset
+    test_loss, test_accuracy = model.evaluate(embeddings_test, y_test_categorical)
 
-    print("Test Loss:", test_loss)
-    print("Test Accuracy:", test_accuracy)
-    print("Below is history")
-    # print(history.history)
+    print("Test Loss on First Dataset:", test_loss)
+    print("Test Accuracy on First Dataset:", test_accuracy)
 
-    # Suppress the UndefinedMetricWarning
-    warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
-
-    ############Evaluating the model##################
-    # Make predictions on the test data
+    # Make predictions on the first test dataset
     y_pred = model.predict(embeddings_test)
-
-    # Convert predicted probabilities to class labels (for binary classification)
-    y_pred_binary = (y_pred > 0.5).astype(int)
-
     y_pred_classes = np.argmax(y_pred, axis=1)
 
-    # Calculate accuracy
+    # Calculate additional metrics, generate reports, and plot confusion matrices for the first dataset
+
+    # ------------------------------
+    # Repeat the same process for the second dataset
     accuracy = accuracy_score(y_test, y_pred_classes)
     print("Accuracy:", accuracy)
 
@@ -170,67 +159,38 @@ def main():
     f1 = f1_score(y_test, y_pred_classes, average='weighted')
     print("F1-score:", f1)
 
-
     # Generate a classification report
     report = classification_report(y_test, y_pred_classes, zero_division=1)
     print("Classification Report:\n", report)
+
+    # Evaluating the model on the second test dataset
+    test_loss2, test_accuracy2 = model.evaluate(embeddings_test2, to_categorical(y_test2, num_classes=num_classes))
+
+    print("Test Loss on Second Dataset:", test_loss2)
+    print("Test Accuracy on Second Dataset:", test_accuracy2)
+
+    # Make predictions on the second test dataset
+    y_pred2 = model.predict(embeddings_test2)
+    y_pred_classes2 = np.argmax(y_pred2, axis=1)
+
+    # Calculate additional metrics, generate reports, and plot confusion matrices for the second dataset
+
+    y_pred_classes = np.argmax(y_pred, axis=1)
+    kappa = calculate_cohens_kappa(y_test, y_pred_classes)
+    print("Cohen's kappa:", kappa)
 
     cm = confusion_matrix(y_test, y_pred_classes)
     plt.figure(figsize=(6, 4))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False)
     plt.xlabel('Predicted labels')
     plt.ylabel('True labels')
-    # plt.title('Safest labels train and test')
+    # plt.title('Confusion Matrix - Test vs test Majority label - Safest label')
+    # # # Save the plot as an image file
+    # plt.savefig('confusion_matrix_test-vs-test-majority-safest.png')
+    plt.title('Confusion Matrix - Test vs test Majority label - Least safest label')
     # Save the plot as an image file
-    # plt.savefig('confusion_matrix-safest.png')
-    # plt.title('Confusion Matrix - Trained on Safest label - Tested on Majority label')
-    # # Save the plot as an image file
-    # plt.savefig('confusion_matrix_safest-maj.png')
-    plt.title('Confusion Matrix - Trained on Safest label - Tested on Least safest label')
-    # # Save the plot as an image file
-    plt.savefig('confusion_matrix_safest-least-safest.png')
+    plt.savefig('confusion_matrix_test-vs-test-majority-least_safest.png')
     plt.close()
-
-
-    # bert_embeddings_file_subset = bert_embeddings_file[:125]
-    # sampled_indices = np.random.choice(len(bert_embeddings_file), size=500, replace=False)
-
-    # Use the sampled indices to select a subset of bert_embeddings_file
-    # bert_embeddings_file_subset = bert_embeddings_file[sampled_indices]
-
-    # print("Shape of X_train:", X_train.shape)
-    # print("Shape of bert_embeddings_file_subset:", bert_embeddings_file_subset.shape)
-    #
-    # n_classes = 3
-    #
-    #
-    # # Create a wrapper function to pass additional arguments
-    # keras_model_wrapper = lambda: create_cnn_model(n_classes, bert_embeddings_file_subset)
-    #
-    # keras_model = KerasClassifier(build_fn=keras_model_wrapper, epochs=20, batch_size=64, verbose=1)
-    #
-    # # print("y_true:", y_test)
-    # print("y_pred:", y_pred)
-    # print("Shape of y_true:", y_test.shape)
-    # print("Shape of y_pred:", y_pred.shape)
-
-    # Cross-validation
-    # y_pred_cv = cross_val_predict(keras_model, bert_embeddings_file_subset, y_test, cv=5)
-    # accuracy_cv = accuracy_score(y_test, y_pred_cv)
-
-    # print("Cross-Validation Accuracy:", accuracy_cv)
-
-    # Calculate Cohen's Kappa
-    y_pred_classes = np.argmax(y_pred, axis=1)
-    kappa = calculate_cohens_kappa(y_test, y_pred_classes)
-    print("Cohen's kappa:", kappa)
-
-    # Assuming 'y' is your target variable
-    num_classes = len(pd.unique(y_test))
-
-    # print("Number of classes:", num_classes)
-    #
-    # print("Shape of bert_embeddings_file_subset:", bert_embeddings_file_subset.shape)
 
 if __name__ == "__main__":
     main()
